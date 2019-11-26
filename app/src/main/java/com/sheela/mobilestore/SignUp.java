e8c619025e58c1f519a0eed8870e341fb631a8aa
sheelabhan <49028679+sheelabhan@users.noreply.github.com>
2019-11-26 14:29:59 +0530

    first commit

---
+++ app/src/main/java/com/sheela/mobilestore/SignUp.java
@@ -0,0 +1,45 @@
+package com.sheela.mobilestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignUp extends AppCompatActivity {
+    private EditText etFullName, etUserName, etEmail, etPassword,etConfirmPassword,etPhoneNo;
+
+    private Button btnRegister;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        etFullName = findViewById(R.id.etFullName);
+        etUserName = findViewById(R.id.etUserName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        etPhoneNo=findViewById(R.id.etPhoneNo);
+        btnRegister = findViewById(R.id.btnRegister);
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent Loginintent = new Intent(SignUp.this, Login.class);
+                startActivity(Loginintent);
+            }
+        });
+
+
+    }
+}
